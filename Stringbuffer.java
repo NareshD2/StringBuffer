ad80c882727b02cb1f22d86917d928954291e9d5
NareshD2 <125354349+NareshD2@users.noreply.github.com>
2023-03-08 12:41:14 +0530

    Add files via upload

---
+++ Stringbuffer.java
@@ -0,0 +1,22 @@
+public class Stringbuffer {
+    static {
+        System.out.println("hello");
+        public int nar(int value){
+            name=value;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer(" ");
+        StringBuffer arn = sb.append("naresh");
+        StringBuffer sn2 = arn.append("homez");
+        sb.replace(0, 3, "hell");
+        sb.insert(2, "hel");
+        sb.delete(0, 3);
+        System.out.println(sn2);
+
+    }
+}
